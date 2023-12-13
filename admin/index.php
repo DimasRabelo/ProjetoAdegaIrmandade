6fa06ac98c0bf720d4d18d460d7334dc2f5414a3
sowie <dimas.rabelosouza@gmail.com>
2023-12-13 17:19:53 -0300

    Criação da Página Dashboard

---
+++ admin/index.php
@@ -0,0 +1,94 @@
+<?php
+
+$pagina = @$_GET['p'];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&family=Secular+One&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="../src/css/reset.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="./css/estilo.css">
+
+
+</head>
+
+<body>
+
+<div class="topomenu">
+        <header class="topoEsquerda">
+            <h1>
+                <img src="img/logoadega.png" alt=" imagem da logo">
+            </h1>
+
+
+            <div class="divlogin">
+                <img class="btnlogin" src="img/btnuser.png" alt="imagem btn Usuario">
+                <h2>Nome:</h2>
+            </div>
+            
+        </header>
+       
+        <main>
+
+          
+
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="">DashBoard</a></li>
+                        <li><a href="">Funcionários</a></li>
+                        <li><a href="">Produtos</a></li>
+                        <li><a href="">Estoque</a></li>
+                        <li><a href="">Vendas</a></li>
+                        <li><a href="">Clientes</a></li>
+                        <li><a href="">E-mails</a></li>
+                        <li><a href="">Relatórios</a></li>
+                        <li><a href="">Ajuda e Suporte</a></li>
+
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="box">
+                <!-- Conteúdo das Páginas -->
+
+
+
+            </div>
+
+
+
+
+
+    </div>
+      </main>
+    <footer class="rodape">
+        <h2>Direitos Reservados Sandini Informática</h2>
+        <h2 class="h2d">Desenvolvido Por Dimas Aparecido Rabelo de Souza</h2>
+    </footer>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="/src/js/slick.min.js"></script>
+    <script src="/src/js/lity.min.js"></script>
+    <script src="/src/js/wow.min.js"></script>
+
+    <script src="/src/js/animacoes.js"></script>
+</body>
