d9e56473c02424cbab37f5d1bcc3e62b8a2bf89a
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2024-01-19 11:47:49 -0300

    update

--- src/Conteudo/Menus/menuindex.php
+++ src/Conteudo/Menus/menuindex.php
@@ -1,42 +1,44 @@
 <header>
-		
-	
-	<nav  class="menu animate__animated animate__fadeInUp"  >
-		
-	
-		
-		
+
+
+
+
+	<nav class="menu animate__animated animate__fadeInUp">
+
+
+
+
 		<ul>
-				<li>
-					<a href="index.php"><img class="btnadega" src="src/imagens/botaoadega.png" alt=" tela de inicio da adega"></a>
-				</li>
+			<li>
+				<a href="index.php"><img class="btnadega" src="src/imagens/botaoadega.png" alt=" tela de inicio da adega"></a>
+			</li>
 
-				<li>
-					<a href="bebidas.php"><img class="btnbebida"  src="src/imagens/botaocerveja.png" alt=" tela de produtos bebidas"></a>
-				</li>
-				<li>
-					<a href="tabacaria.php"><img class="btnpadding" src="src/imagens/botaonarguile.png" alt=" tela de tabacaria"></a>
+			<li>
+				<a href="bebidas.php"><img class="btnbebida" src="src/imagens/botaocerveja.png" alt=" tela de produtos bebidas"></a>
+			</li>
+			<li>
+				<a href="tabacaria.php"><img class="btnpadding" src="src/imagens/botaonarguile.png" alt=" tela de tabacaria"></a>
 
-				</li>
+			</li>
 
-				<li>
-					<a href="galeria.php"><img class="btnpadding" src="src/imagens/botao galeria.png" alt=" tela Galeria de fotos e videos"></a>
-				</li>
+			<li>
+				<a href="galeria.php"><img class="btnpadding" src="src/imagens/botao galeria.png" alt=" tela Galeria de fotos e videos"></a>
+			</li>
 
-				<li>
-					<a href="contato.php"><img class="btnpadding"  src="src/imagens/botaocontato.png" alt=" tela de Contatos"></a>
-				</li>
+			<li>
+				<a href="contato.php"><img class="btnpadding" src="src/imagens/botaocontato.png" alt=" tela de Contatos"></a>
+			</li>
 
-				<li>
-					<a href="usuario.php"><img class="btnpadding" src="src/imagens/botaouser.png" alt=" tela pagina de usuarios"></a>
-				</li>
-
-
-			</ul>
-
-		</nav>
+			<li>
+				<a href="usuario.php"><img class="btnpadding" src="src/imagens/botaouser.png" alt=" tela pagina de usuarios"></a>
+			</li>
 
 
+		</ul>
+
+	</nav>
 
 
-	</header>
+
+
+</header>
