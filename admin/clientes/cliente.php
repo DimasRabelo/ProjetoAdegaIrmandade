c6073a41af9320ca9c964b4ea0a2ba4b8b15e470
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2024-01-22 09:46:33 -0300

    update

--- admin/clientes/cliente.php
+++ admin/clientes/cliente.php
@@ -9,4 +9,6 @@ if ($pagina == NULL) {
     if($pagina == 'cadastrar') { require_once('cadastrar.php');}
     if($pagina ==  'atualizar') { require_once('atualizar.php');}    
     if($pagina == 'desativar' ) { require_once('desativar.php');}
+    if($pagina == 'ativar' ) { require_once('ativar.php');}
 }
+
