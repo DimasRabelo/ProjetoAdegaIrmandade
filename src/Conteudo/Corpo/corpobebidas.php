fb6af81580aaca0edd04b9b10f7af8082a8fe040
sowie <dimas.rabelosouza@gmail.com>
2024-01-25 15:07:40 -0300

    update

--- src/Conteudo/Corpo/corpobebidas.php
+++ src/Conteudo/Corpo/corpobebidas.php
@@ -13,49 +13,49 @@
 
             <div class="Cervejas">
                 <div>
-                    <img src="src/imagens/cerveja220/skol269.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/skol269.png" alt="">
                     <p>Skol 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/cerveja220/Amstel269ml.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/Amstel269ml.png" alt="">
                     <p>Amstel 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/cerveja220/brahmaduplomalte269ml.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/brahmaduplomalte269ml.png" alt="">
                     <p>Brahma Duplo Malte 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/cerveja220/coronalongneck.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/coronalongneck.png" alt="">
                     <p>Corona Long Neck 330ML</p>
                 </div>
 
 
                 <div>
-                    <img src="src/imagens/cerveja220/heinekenlongneck.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/heinekenlongneck.png" alt="">
                     <p>Heineken Long Neck 330ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/cerveja220/itaipava269ml.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/itaipava269ml.png" alt="">
                     <p>Itaipava 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/cerveja220/original269ml.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/original269ml.png" alt="">
                     <p>Original 269ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/cerveja220/spaten269.png" alt="">
+                    <img src="src/imagens/bebidas/cerveja220/spaten269.png" alt="">
                     <p>Spaten 269ML</p>
                 </div>
 
             </div>
             <div class="banner">
-                <img src="src/imagens/bannerbalde.png" alt="">
+                <img src="src/imagens/banner/bannerbalde.png" alt="">
             </div>
         </section>
     </div>
@@ -78,29 +78,29 @@
 
             <section>
                 <div>
-                    <img src="src/imagens/refrigerantes220/bally.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/bally.png" alt="">
                     <p>Energético Bally 2 Litros</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/coca-cola-2-litros.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/coca-cola-2-litros.png" alt="">
                     <p>Coca Cola 2 Litros</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/cocacola350ml.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/cocacola350ml.png" alt="">
                     <p>Coca Lata 350ML</p>
                 </div>
 
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/dolly.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/dolly.png" alt="">
                     <p>Dolly 2 Litros</p>
                 </div>
 
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/fanta2litros.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/fanta2litros.png" alt="">
                     <p>Fanta 2 litros</p>
                 </div>
             </section>
@@ -110,27 +110,27 @@
 
             <section>
                 <div>
-                    <img src="src/imagens/refrigerantes220/fanta350ml.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/fanta350ml.png" alt="">
                     <p>Fanta Lata 350ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/gatorade.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/gatorade.png" alt="">
                     <p>Gatorade 500ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/gelodecoco.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/gelodecoco.png" alt="">
                     <p>Gelo de Coco</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/gelodesabormelancia.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/gelodesabormelancia.png" alt="">
                     <p>Gelo Sabor Melancia</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/guaraviton.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/guaraviton.png" alt="">
                     <p>Guaraviton 500ML</p>
                 </div>
             </section>
@@ -138,27 +138,27 @@
 
             <section>
                 <div>
-                    <img src="src/imagens/refrigerantes220/h20.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/h20.png" alt="">
                     <p>H2O 500ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/images.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/images.png" alt="">
                     <p>Chop de Vinho 600ML</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/redbul.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/redbul.png" alt="">
                     <p>Red Bull 250ML </p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/vibe.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/vibe.png" alt="">
                     <p>Energético Vibe 2 Litros</p>
                 </div>
 
                 <div>
-                    <img src="src/imagens/refrigerantes220/pepsi.png" alt="">
+                    <img src="src/imagens/bebidas/refrigerantes220/pepsi.png" alt="">
                     <p>Pepsi 2 Litros</p>
                 </div>
             </section>
@@ -184,22 +184,22 @@
         <div class="Destilados">
 
             <div>
-                <img src="src/imagens/fortes220/ballantines.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/ballantines.png" alt="">
                 <p>Ballantines</p>
             </div>
 
             <div>
-                <img src="src/imagens/fortes220/black label.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/black label.png" alt="">
                 <p>Black Label</p>
             </div>
 
             <div>
-                <img src="src/imagens/fortes220/gin rocks.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/gin rocks.png" alt="">
                 <p>Gin Rocks</p>
             </div>
 
             <div>
-                <img src="src/imagens/fortes220/gininvictus.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/gininvictus.png" alt="">
                 <p>Gin Invictus</p>
             </div>
 
@@ -208,29 +208,29 @@
     </div>
 
     <div class="bannerdestilado1">
-        <img src="src/imagens/bannerdestilado.png" alt="">
+        <img src="src/imagens/banner/bannerdestilado.png" alt="">
     </div>
 
     <div class="site">
         <div class="Destilados2">
 
             <div>
-                <img src="src/imagens/fortes220/jackdaniels.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/jackdaniels.png" alt="">
                 <p>Jack Daniels</p>
             </div>
 
             <div>
-                <img src="src/imagens/fortes220/redlabel.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/redlabel.png" alt="">
                 <p>Red Label</p>
             </div>
 
             <div>
-                <img src="src/imagens/fortes220/whitehorse.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/whitehorse.png" alt="">
                 <p>White Horse</p>
             </div>
 
             <div>
-                <img src="src/imagens/fortes220/taquery.png" alt="">
+                <img src="src/imagens/bebidas/fortes220/taquery.png" alt="">
                 <p>Taquery</p>
 
             </div>
