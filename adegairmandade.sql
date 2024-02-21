af47e0c9582eea4016281c2646553e4e7cd7b7bc
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2024-02-21 09:13:34 -0300

    Update adegairmandade.sql

--- adegairmandade.sql
+++ adegairmandade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30/01/2024 às 15:20
+-- Tempo de geração: 20/02/2024 às 13:59
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.0.28
 
