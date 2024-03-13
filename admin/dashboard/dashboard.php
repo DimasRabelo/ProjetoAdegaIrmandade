5f83f72c043e0352f2c469dccc186fad48972141
sowie <dimas.rabelosouza@gmail.com>
2024-03-12 21:39:39 -0300

    Update dashboard.php

--- admin/dashboard/dashboard.php
+++ admin/dashboard/dashboard.php
@@ -1,25 +1,37 @@
+<html lang="pt-BR">
+
+<head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik&family=Secular+One&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="./src/css/reset.css" />
+
+
+
+
+
+</head>
+
+<body>
+
+        <main>
+
+                <button><a href="index.php?p=galeria">Galeria </a></button>
+                <button><a href="index.php?p=banner">Banners </a></button>
+        </main>
 
 
 
 
 
 
-<html>
-<body >
-   
-        <button><a href="index.php?p=galeria">Galeria </a></button>
-        <button><a href="index.php?p=banner">Banners </a></button>
-        
-    
+
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
-
